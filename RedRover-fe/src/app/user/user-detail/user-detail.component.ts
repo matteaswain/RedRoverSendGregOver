f4dfe3e725374bba8c7d75c474aedcbc673102d3
Charlie Traylor <ctraylor@tql.com>
2021-07-08 16:13:39 -0400

    status wont update when clicked

--- RedRover-fe/src/app/user/user-detail/user-detail.component.ts
+++ RedRover-fe/src/app/user/user-detail/user-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { SystemService } from 'src/app/misc/system.service';
 import { User } from '../user.class';
 import { UserService } from '../user.service';
@@ -10,7 +11,10 @@ import { UserService } from '../user.service';
 })
 export class UserDetailComponent implements OnInit {
 
-  constructor(private syssvc: SystemService, private usersvc: UserService) { }
+  user: User = new User();
+  id: number = 0;
+
+  constructor(private syssvc: SystemService, private usersvc: UserService,private router: Router, private activatedRoute: ActivatedRoute) { }
 
   loggedInUser!: User;
   redLit: string = "assets/images/RRL-red-lit.png";
@@ -23,24 +27,43 @@ export class UserDetailComponent implements OnInit {
   yellow: string = this.yellowUnlit;
   green: string = this.greenLit;
 
+
+  refresh(): void{
+    this.id = this.activatedRoute.snapshot.params.id;
+    this.usersvc.getByPk(this.id).subscribe(
+      res => {console.debug("Success", res);
+    this.user = res;
+  },
+  err =>{console.error(err);}
+    )
+  }
+
+
   lightSwitch(light: string): void{
     switch(light){
       case('red'): 
-      this.usersvc.statusRed(this.loggedInUser);
+      this.usersvc.statusRed(this.loggedInUser).subscribe(
+        res => console.log("Updated to red!"),
+        err => console.error(err)
+      );
       this.red = this.redLit;
       this.yellow = this.yellowUnlit;
       this.green = this.greenUnlit;
       break;
       case('yellow'): 
-      this.usersvc.statusYellow(this.loggedInUser)
+      this.usersvc.statusYellow(this.loggedInUser).subscribe(
+        res => console.log("Updated to yellow!"),
+        err => console.error(err))
       this.yellow = this.yellowLit;
       this.red = this.redUnlit;
-      this.green = this.greenUnlit;
+      this.green = this.greenUnlit;      
       break;
       case('green'): 
-      this.usersvc.statusGreen(this.loggedInUser)
+      this.usersvc.statusGreen(this.loggedInUser).subscribe(
+        res => console.log("Updated to green!"),
+        err => console.error(err))
       this.lightReset();
-    }
+    }    
     console.log(this.loggedInUser);
   }
 
@@ -51,7 +74,13 @@ export class UserDetailComponent implements OnInit {
   }
 
   ngOnInit(): void {
-   if(this.syssvc.loggedInUser != null) {this.loggedInUser = this.syssvc.loggedInUser}
+   if(this.syssvc.loggedInUser !== null) {this.loggedInUser = this.syssvc.loggedInUser}
+   else { const routeParams = this.activatedRoute.snapshot.paramMap;
+  const id = Number(routeParams.get('id'))
+  this.usersvc.getByPk(id).subscribe(
+    res => this.loggedInUser = res,
+    err => console.error(err)
+  )}
   }
 
 }
